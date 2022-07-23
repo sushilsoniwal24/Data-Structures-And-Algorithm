5989c4f42d000323eedb5419e7ed5f5e7471734d
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-07-23 11:51:06 +0530

    1-D Array In Java

--- Arrays/Array_1D/Array_Basics/Intro.java
+++ Arrays/Array_1D/Array_Basics/Intro.java
@@ -1,15 +1,24 @@
 package Arrays.Array_1D.Array_Basics;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Intro {
     public static void main(String[] args) {
+        met1();
+        met2();
+    }
+
+    /* 2 ways to declare & initialize array. */
+
+    // ********** Method-01 **********
+    public static void met1() {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the size of your array:- ");
         int size = in.nextInt();
 
-        int[] arr = new int[size]; // 0-Based Index.
+        int[] arr = new int[size];
 
         System.out.println("Enter the elements of your array till the entered size");
         for (int index = 0; index < size; index++) {
@@ -21,8 +30,18 @@ public class Intro {
             System.out.print(arr[index] + " ");
         }
 
-        in.close();
+        // Line Break.
+        System.out.println();
 
+        in.close();
+    }
+
+    // ********** Method-02 **********
+    public static void met2() {
+        int[] arr = { 1, 2, 3 };
+
+        System.out.print("Your array is:- ");
+        System.out.println(Arrays.toString(arr));
     }
 
 }
