5da7778cc7727e0addfdefbd1bec0f7ac8dad527
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-07-28 08:23:19 +0530

    Palindrome string in java

--- Strings/String_Questions/Palindrome_String.java
+++ Strings/String_Questions/Palindrome_String.java
@@ -3,12 +3,12 @@ package Strings.String_Questions;
 public class Palindrome_String {
     public static void main(String[] args) {
         String str = "racecar";
-        System.out.println(isPalindrome(str));
+        System.out.println(isPalindrome2(str));
 
     }
 
-    // Time Complexity = O(n) & Space Complexity = O(n).
-    public static boolean isPalindrome(String str) {
+    // Method - 01 => Time Complexity = O(n) & Space Complexity = O(n).
+    public static boolean isPalindrome1(String str) {
         int str_len = str.length() - 1;
         StringBuilder sb = new StringBuilder();
 
@@ -24,5 +24,21 @@ public class Palindrome_String {
         } else {
             return false;
         }
+    }
+    /* ***************************************************************** */
+
+    // Method - 01 => Time Complexity = O(n) & Space Complexity = O(1).
+    public static boolean isPalindrome2(String str) {
+        int start = 0, end = str.length() - 1;
+
+        while (start <= end) {
+            if (str.charAt(start) == str.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
