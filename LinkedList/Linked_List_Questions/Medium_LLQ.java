40fba576a282f10c6e9c9722d5a7a8b29628c370
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-08-16 17:24:49 +0530

    Remove Node

--- LinkedList/Linked_List_Questions/Medium_LLQ.java
+++ LinkedList/Linked_List_Questions/Medium_LLQ.java
@@ -29,8 +29,9 @@ public class Medium_LLQ {
         }
         return null;
     }
+    // ------------------------------------------------------------------------------------------------
 
-    /*** (1) Reorder List. ***/
+    /*** (2) Reorder List. ***/
     /*
      * (1) Link:- https://leetcode.com/problems/reorder-list/
      * (2) Time Complexity = O(n).
@@ -59,8 +60,82 @@ public class Medium_LLQ {
             head1.next = null;
         }
     }
+    // ------------------------------------------------------------------------------------------------
 
-    // To Get Middle Of The Linked List.
+    /*** (3) Reverse Linked List II. ***/
+    /*
+     * (1) Link:- https://leetcode.com/problems/reverse-linked-list-ii/
+     * (2) Time Complexity = O(n).
+     * (3) Space Complexity = O(1).
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode prev = null, current = head;
+        int count = 1;
+        while (count != left) {
+            prev = current;
+            current = current.next;
+            count++;
+        }
+
+        ListNode start = current;
+        while (count != right) {
+            current = current.next;
+            count++;
+        }
+
+        ListNode rest = current.next;
+        current.next = null;
+
+        ListNode newHead = reverseList(start);
+
+        if (prev != null) {
+            prev.next = newHead;
+        }
+
+        current = newHead;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = rest;
+
+        if (left == 1) {
+            return newHead;
+        }
+        return head;
+    }
+    // ------------------------------------------------------------------------------------------------
+
+    /*** (4) Remove Nth Node From End of List. ***/
+    /*
+     * (1) Link:- https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+     * (2) Time Complexity = O(n).
+     * (3) Space Complexity = O(1).
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode first = head, second = head;
+        for (int index = 0; index < n; index++) {
+            first = first.next;
+        }
+
+        if (first == null) {
+            return head.next;
+        }
+
+        while (first.next != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+        return head;
+    }
+    // ------------------------------------------------------------------------------------------------
+
+    // Middle Of The Linked List.
     public ListNode middleNode(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
@@ -71,7 +146,7 @@ public class Medium_LLQ {
         return slow;
     }
 
-    // Reverse List From Mid Till End.
+    // Reverse The Linked List.
     public ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode prev = null;
