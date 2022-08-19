47b70a880209893b17e253d37c2d8f0c0cb2cc3a
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-08-19 09:40:50 +0530

    Traversals Of A Binary Tree

---
+++ Tree/Binary_Tree/Tree_Traversals/PreOrder_Traversal.java
@@ -0,0 +1,40 @@
+package Tree.Binary_Tree.Tree_Traversals;
+
+public class PreOrder_Traversal {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    /* Recursive PreOrder Traversal:- root->left->right. */
+    public static void recPreOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        recPreOrder(root.left);
+        recPreOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(4);
+        root.left = new Node(9);
+        root.right = new Node(7);
+        root.left.left = new Node(12);
+        root.left.right = new Node(15);
+        root.left.right.right = new Node(14);
+        root.right.left = new Node(11);
+        root.right.right = new Node(10);
+        root.right.right.left = new Node(20);
+
+        recPreOrder(root);
+    }
+}
