667f63b6c22992c8ac9c4bf9355525d53fadb911
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-08-25 13:15:11 +0530

    Node class

---
+++ Tree/Binary_Tree/Tree_Questions/Node.java
@@ -0,0 +1,11 @@
+package Tree.Binary_Tree.Tree_Questions;
+
+public class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
