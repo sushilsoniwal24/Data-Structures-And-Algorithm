0695cd6c85684def1094b47cee9c503e51a910c7
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-09-09 09:20:23 +0530

    Node Class

---
+++ Heaps/Heap_Questions/Node.java
@@ -0,0 +1,11 @@
+package Heaps.Heap_Questions;
+
+public class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
