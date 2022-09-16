f91a8241fd583d5fe4d7917923e15d380e1fa4ca
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-09-16 12:57:33 +0530

    Adjacency Matrix Implementation

--- Graph/Introduction_To_Graphs/Adjacency_Matrix.java
+++ Graph/Introduction_To_Graphs/Adjacency_Matrix.java
@@ -1,31 +1,60 @@
 package Graph.Introduction_To_Graphs;
 
-import java.util.Scanner;
-
 public class Adjacency_Matrix {
-    static Scanner in = new Scanner(System.in);
+    int vertices;
+    int[][] matrix;
 
-    /* (1) Adjacency Matrix Representation Of Graph. */
-    public static void adjMat(int n, int m) {
-        int[][] graph_mat = new int[n + 1][n + 1];
-        for (int i = 0; i < m; i++) {
-            int source = in.nextInt();
-            int destination = in.nextInt();
+    Adjacency_Matrix(int v) {
+        vertices = v;
+        matrix = new int[v][v];
+    }
 
-            graph_mat[source][destination] = 1;
-            graph_mat[destination][source] = 1;
+    // ! Add An Edge :- O(1).
+    public void addEdge(int source, int destination) {
+        matrix[source][destination] = 1;
+        matrix[destination][source] = 1;
+    }
+
+    // ! Delete An Edge :- O(1).
+    public void deleteEdge(int source, int destination) {
+        matrix[source][destination] = 0;
+        matrix[destination][source] = 0;
+    }
+
+    // ! Print The Graph.
+    public void printGraph() {
+        System.out.println("Representation of Graph in the form of Adjacency Matrix: ");
+
+        // Todo :- Print In The Form Of A Matrix.
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 0; j < vertices; j++) {
+                System.out.print(matrix[i][j] + "   ");
+            }
+            System.out.println();
         }
 
-        // ! Print In The Form Of A Matrix.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(graph_mat[i][j] + " ");
+        // Todo :- Print In The Form Of Vertices Connection.
+        for (int i = 0; i < vertices; i++) {
+            System.out.print("Vertex " + i + " is connected to: ");
+            for (int j = 0; j < vertices; j++) {
+                if (matrix[i][j] == 1) {
+                    System.out.print(j + " ");
+                }
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        adjMat(4, 5);
+        Adjacency_Matrix adj = new Adjacency_Matrix(5);
+        adj.addEdge(0, 1);
+        adj.addEdge(0, 4);
+        adj.addEdge(1, 2);
+        adj.addEdge(1, 3);
+        adj.addEdge(1, 4);
+        adj.addEdge(2, 3);
+        adj.addEdge(3, 4);
+
+        adj.printGraph();
     }
 }
