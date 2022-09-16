ce9bd2f52450aa60887ced7ade696c6e63691dc4
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-09-16 12:58:09 +0530

    Adjacency List Implementation

--- Graph/Introduction_To_Graphs/Adjacency_List.java
+++ Graph/Introduction_To_Graphs/Adjacency_List.java
@@ -3,36 +3,51 @@ package Graph.Introduction_To_Graphs;
 import java.util.ArrayList;
 
 public class Adjacency_List {
+    int vertices;
 
-    /* Method To Add Edges In A Graph. */
+    // ! Add An Edge :- O(1).
     public static void addEdge(ArrayList<ArrayList<Integer>> adj, int source, int destination) {
+
+        // * Adding an edge from source to destination
         adj.get(source).add(destination);
+
+        // * Adding an edge from destination to source
         adj.get(destination).add(source);
     }
 
-    /* Method To Print Graph. */
+    // ! Delete An Edge :- O(1).
+    public static void deleteEdge(ArrayList<ArrayList<Integer>> adj, int source, int destination) {
+        adj.get(source).remove(destination);
+    }
+
+    // ! Print The Graph.
     public static void printGraph(ArrayList<ArrayList<Integer>> adj) {
+        System.out.println("Adjacency List representation of Graph: ");
+
         for (int i = 0; i < adj.size(); i++) {
-            System.out.println("\nAdjacency list of vertex " + i);
-            System.out.print("head");
+            System.out.println("Adjacency List of vertex " + i);
+            System.out.print(i + " -> ");
             for (int j = 0; j < adj.get(i).size(); j++) {
-                System.out.print(" -> " + adj.get(i).get(j));
+                System.out.print(adj.get(i).get(j) + " ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int V = 4;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+        int vertices = 5;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(vertices);
 
-        for (int i = 0; i < V; i++)
+        for (int i = 0; i < vertices; i++) {
             adj.add(new ArrayList<Integer>());
+        }
 
         addEdge(adj, 0, 1);
-        addEdge(adj, 0, 2);
+        addEdge(adj, 0, 4);
         addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
         addEdge(adj, 2, 3);
+        addEdge(adj, 3, 4);
 
         printGraph(adj);
     }
